8eebf3873305874c9a9faa8049477e869ca9c221
Seulkiming <conductor@tutti.kr>
2014-08-23 19:36:33 +0900

    created __init__.py containing Facebook OAuth

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,18 @@
 from flask import Flask
+from flask_oauth import flask_OAuth
+from app import settings
 
 app = Flask(__name__)
+app.config.from_object("app.settings.Production")
 
-from app import views
+facebook = oauth.remote_app('facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key=settings.Production.FACEBOOK_APP_ID
+    consumer_secret=settings.Production.FACEBOOK_APP_SECRET
+    request_token_params={'scope': 'email'}
+    )
+
+from app import controllers
