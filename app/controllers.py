962220a2915e7b048ef735eb4730b80d1babe045
Seulkiming <conductor@tutti.kr>
2014-08-25 19:49:04 +0900

    edited some functions, variables names

--- app/controllers.py
+++ app/controllers.py
@@ -9,30 +9,38 @@ import pusher
 @app.route('/')
 
 @app.route('/index')
-def chat():
-    return render_template("chat.html")
+def index():
+    return render_template("login.html")
 
-@app.route("/login", methods=["POST"])
+@app.route("/login")
 def login():
-    return facebook.authorize(callback = url_for("facebook_authorized",
-        next=request.args.get("next") or request.referrer or None, _external=True))
+    return facebook.authorize(callback = url_for("oauth_authorized",
+        next=request.args.get("next") or request.referrer or None,
+        _external=True))
 
 @app.route("/login/authorized")
 @facebook.authorized_handler
-def facebook_authorized(resp):
+def oauth_authorized(resp):
     if resp is None:
         # return "Access denied: reason=%s error=%s" % (
         #         request.args["error_reason"],
         #         request.args["error_description"]
         #     )
         return redirect(url_for("chat.html"))
-    session["oauth_token"] = (resp["access_token"], "")
+    session["facebook_token"] = (
+        resp["oauth_token"],
+        resp["oauth_token_secret"]
+    )
     me = facebook.get("/me")
 
     session["username"] = me.data("name")
     session["user_id"] = me.data("id")
     return redirect(url_for("chat"))
 
+# OAuth uses a token and a secret
+# to figure out who is connecting to the remote application.
+# After authentication/authorization, this information is passed to a function on your side
+# and it is your responsibility to remember it.
 @facebook.tokengetter
 def get_facebook_oauth_token():
     return session.get("oauth_token")
