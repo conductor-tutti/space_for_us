557405a9bea4532ffe25cffb8ead8ca443e90a34
Seulkiming <conductor@tutti.kr>
2014-08-25 21:23:08 +0900

    I saw same error continuously(TypeError: dict is not callable blah blah) and that was from my incorrect syntax, using () to a dictionary. Use []!

--- app/controllers.py
+++ app/controllers.py
@@ -1,10 +1,12 @@
 # -*- coding: UTF-8 -*-
 from flask import render_template, session, request, Flask, jsonify, redirect, url_for
+from flask_oauth import OAuth
 from app import app, facebook
 from datetime import datetime
 from time import time
 import pusher
 
+import logging
 
 @app.route('/')
 
@@ -14,33 +16,27 @@ def index():
 
 @app.route("/login")
 def login():
-    return facebook.authorize(callback = url_for("oauth_authorized",
+    return facebook.authorize(callback = url_for("facebook_authorized",
         next=request.args.get("next") or request.referrer or None,
         _external=True))
 
 @app.route("/login/authorized")
 @facebook.authorized_handler
-def oauth_authorized(resp):
+def facebook_authorized(resp):
     if resp is None:
-        # return "Access denied: reason=%s error=%s" % (
-        #         request.args["error_reason"],
-        #         request.args["error_description"]
-        #     )
         return redirect(url_for("chat.html"))
-    session["facebook_token"] = (
-        resp["oauth_token"],
-        resp["oauth_token_secret"]
-    )
+    session["oauth_token"] = (resp["access_token"], "")
     me = facebook.get("/me")
+    logging.debug(me.data)
 
-    session["username"] = me.data("name")
-    session["user_id"] = me.data("id")
+    session["username"] = me.data["name"]
+    session["user_id"] = me.data["id"]
     return redirect(url_for("chat"))
 
-# OAuth uses a token and a secret
-# to figure out who is connecting to the remote application.
-# After authentication/authorization, this information is passed to a function on your side
-# and it is your responsibility to remember it.
 @facebook.tokengetter
 def get_facebook_oauth_token():
     return session.get("oauth_token")
+
+@app.route("/chat")
+def chat():
+    return render_template("chat.html")
